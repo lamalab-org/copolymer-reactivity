4b0ce6c3553ae1ab7091ec409173d16b9ee9282d
maraw <mara.wilhelmi@uni-jena.de>
2023-12-18 10:14:36 +0100

    polymer reactivity project

--- extraction.py
+++ extraction.py
@@ -1,21 +1,25 @@
-import openai
+from openai import OpenAI
 import os
+import json
 
-openai.api_key = "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"
-input_folder = 'test/markdown_output'
-output_folder = 'test/model_output'
+input_folder = 'markdown_output'
+output_folder = 'model_output'
+
+client = OpenAI(
+    api_key="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"
+)
 
 prompt = ""
 
-for filename in os.listdir(input_folder):
+for i, filename in enumerate(os.listdir(input_folder)):
     if filename.endswith(".mmd"):
         file_path = os.path.join(input_folder, filename)
         with open(file_path, 'r', encoding='utf-8') as file:
             file_content = file.read()
         prompt = (f"""Here is the content of the file {filename}: {file_content}
-        Extract the polymerization information from each polymerization and report it as a table with the following 
-        columns and information
-        
+        Extract the polymerization information from each polymerization and report it in json format. 
+        Extract the following information:
+
         source
         polymerization reaction
         involved monomers
@@ -26,11 +30,21 @@ for filename in os.listdir(input_folder):
         confidence interval of polymerization reaction constant r1 and r2
          
         If the information is not provided put NA. If there are multiple polymerization's with different parameters 
-        report as a separate line.""")
-        response = openai.Completion.create(
-            engine="text-davinci-003",
-            prompt=prompt,
-            max_tokens=150,
-            temperature=0.5
+        report as a separate polymerization.""")
+
+        completion = client.chat.completions.create(
+            model="gpt-3.5-turbo-1106",
+            response_format={"type": "json_object"},
+            messages=[
+                {"role": "system",
+                 "content": "You are a scientific assistant, extracting important information about polymerization "
+                            "conditions out of text."},
+                {"role": "user", "content": prompt}
+            ]
         )
-        print(response)
+        message_content = completion.choices[0].message.content
+        polymerization_data = json.loads(message_content)
+        print(message_content)
+        output_name = os.path.join(output_folder, f'output_data{i+1}.json')
+        with open(output_name, 'w') as file:
+            json.dump(polymerization_data, file, indent=4)
