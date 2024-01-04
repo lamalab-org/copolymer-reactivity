7373322d52b9b0b154ed79eee9e23c543d7c4594
Kevin Maik Jablonka <kevin.jablonka@epfl.ch>
2024-01-04 21:00:28 +0100

    find matching combinations

--- tests/test_analyzer.py
+++ tests/test_analyzer.py
@@ -1,4 +1,8 @@
-from copolextractor.analyzer import get_total_number_of_combinations, find_matching_reaction
+from copolextractor.analyzer import (
+    get_total_number_of_combinations,
+    find_matching_reaction,
+    find_matching_combination,
+)
 import pytest
 
 
@@ -55,3 +59,74 @@ def test_get_total_number_of_combinations():
 )
 def test_find_matching_reaction(data, monomers, expected):
     assert find_matching_reaction(data, monomers) == expected
+
+
+@pytest.mark.parametrize(
+    "data,ground_truth,expected,expected_score",
+    [
+        (
+            [
+                {
+                    "polymerization_type": "radical",
+                    "solvent": "water",
+                    "temperature": 10,
+                    "method": "A",
+                },
+                {
+                    "polymerization_type": "radical",
+                    "solvent": "ethanol",
+                    "temperature": 20,
+                    "method": "A",
+                },
+            ],
+            ("radical", "water", 10, "A"),
+            0,
+            1,
+        ),
+        (
+            [
+                {
+                    "polymerization_type": "radical (A)",
+                    "solvent": "water",
+                    "temperature": 10,
+                    "method": "A",
+                },
+                {
+                    "polymerization_type": "radical",
+                    "solvent": "ethanol",
+                    "temperature": 20,
+                    "method": "A",
+                },
+            ],
+            ("radical", "water", 10, "A"),
+            0,
+            0.9,
+        ),
+        (
+            [
+                {
+                    "polymerization_type": "radical (A)",
+                    "solvent": "oxidane",
+                    "temperature": 10,
+                    "method": "A",
+                },
+                {
+                    "polymerization_type": "radical",
+                    "solvent": "ethanol",
+                    "temperature": 20,
+                    "method": "A",
+                },
+            ],
+            ("radical", "water", 10, "A"),
+            0,
+            0.9,
+        )
+    ],
+)
+def test_find_matching_combination(data, ground_truth, expected, expected_score):
+    idx, score =  find_matching_combination(data, *ground_truth)
+    assert idx == expected
+    if expected_score != 1:
+        assert score <= 1
+    else:
+        assert score == expected_score
