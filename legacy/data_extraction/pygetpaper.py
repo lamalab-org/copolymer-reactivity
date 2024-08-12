7b6941d8802111392c3238ea3e978881dc0a365e
maraw <mara.wilhelmi@uni-jena.de>
2024-08-12 14:14:46 +0200

    add XML cleaning + content extraction; start monomer property calculation

--- legacy/data_extraction/pygetpaper.py
+++
@@ -1,5 +0,0 @@
-from pygetpapers import Pygetpapers
-
-
-pygetpapers_call=Pygetpapers()
-pygetpapers_call.run_command(query='"Copolymerization" AND "reactivity"', limit=2500, output='files_pygetpaper', xml=True, update=True)
